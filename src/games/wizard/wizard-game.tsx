179b4fed78e32f0f2417ff4fe56f4d92b9dd1b7d
Stephen <code@cptnfizzbin.ca>
2025-12-06 22:23:50 -0500

    fix: yarn fix

--- src/games/wizard/wizard-game.tsx
+++ src/games/wizard/wizard-game.tsx
@@ -2,17 +2,23 @@ import { Group as GroupIcon } from "@mui/icons-material"
 import MoreVertIcon from "@mui/icons-material/MoreVert"
 import PlayArrowIcon from "@mui/icons-material/PlayArrow"
 import { Menu, MenuItem, Stack, Typography } from "@mui/material"
+import IconButton from "@mui/material/IconButton"
+import Paper from "@mui/material/Paper"
 import { type FC, useState } from "react"
 import { LeaderboardButton } from "@/games/wizard/leaderboard/leaderboard-button"
 import { PlayerList } from "@/games/wizard/players/player-list"
 import { PlayerManagementDialog } from "@/games/wizard/players/player-management-dialog"
 import { EndRoundButton } from "@/games/wizard/round/end-round-button"
 import { startNewGame } from "@/games/wizard/wizard.actions"
-import { selectCurrentRound, selectTotalBids } from "@/games/wizard/wizard.selectors"
-import { useWizardDispatch, useWizardSelector } from "@/games/wizard/wizard-store"
+import {
+  selectCurrentRound,
+  selectTotalBids,
+} from "@/games/wizard/wizard.selectors"
+import {
+  useWizardDispatch,
+  useWizardSelector,
+} from "@/games/wizard/wizard-store"
 import { WizardStoreProvider } from "@/games/wizard/wizard-store-provider"
-import IconButton from "@mui/material/IconButton"
-import Paper from "@mui/material/Paper"
 
 const WizardGameContent: FC = () => {
   const [dialogOpen, setDialogOpen] = useState(false)
@@ -44,10 +50,7 @@ const WizardGameContent: FC = () => {
       <Stack direction={"row"} gap={1} justifyContent={"flex-end"}>
         <LeaderboardButton />
 
-        <IconButton
-          size="small"
-          onClick={handleMenuOpen}
-        >
+        <IconButton size="small" onClick={handleMenuOpen}>
           <MoreVertIcon />
         </IconButton>
 
